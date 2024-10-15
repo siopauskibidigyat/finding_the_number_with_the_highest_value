4874c41abede8fb3fcac1f506f0297721006e507
unknown <klarenz0620@gmail.com>
2024-10-15 21:24:53 +0800

    di q po na-save, sori hehe

---
+++ highest_number_among_the_five.py
@@ -0,0 +1,21 @@
+#define the 5 variables
+    #identify the numbers as the input
+#compare var1 and var2
+#if var1 is greater than var2
+    #compare var1 to var3
+    #if var1 is greater than var3
+    #compare var1 to var4
+    #if var1 is greater than var4
+    #compare var1 to var 5
+    #if var1 is greater than var 5
+    #print var1
+    #if not print var5 
+#else if var1 is less than var2
+    #compare var2 to var3
+    #if var2 is greater than var3
+    #compare var2 to var4
+    #if var2 is greater than var4
+    #compare var2 to var5
+    #if var2 is greater than var5
+    #print var2
+    #if not print var 5
