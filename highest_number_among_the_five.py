83aab1f075e87ff13a9f3c05fe897e8ac28d2864
unknown <klarenz0620@gmail.com>
2024-10-15 21:49:19 +0800

    perstep

--- highest_number_among_the_five.py
+++ highest_number_among_the_five.py
@@ -1,5 +1,18 @@
 #define the 5 variables
     #identify the numbers as the input
+def find_highest(num1, num2, num3, num4, num5):
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    num3 = int(input("Enter another number: "))
+    num4 = int(input("Enter another number: "))
+    num5 = int(input("Enter another number: "))
+
+    num1 = int(num1)
+    num2 = int(num2)
+    num3 = int(num3)
+    num4 = int(num4)
+    num5 = int(num5)
+res = find_highest(num1=0, num2=0, num3=0, num4=0, num5=0)
 #compare var1 and var2
 #if var1 is greater than var2
     #compare var1 to var3
